0d3ae82e5ce83dbfec7b020df0d310fa41f4c0a9
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2025-03-14 12:30:38 +0800

    add Contact component and update About page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div>Content inside the about page</div>;
+  return <div>Im am about page</div>;
 };
 
 export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <div>Im a contact page</div>;
+};
+
+export default Contact;
