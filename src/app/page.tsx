60b87fecc38175cc0e9c818f0c26f915f98f3ea2
Afiq-content <afiqconten.t@gmail.com>
2025-03-19 08:33:17 +0800

    add Home, Dashboard, Profile, and Settings components with basic structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This landing page of this website</p>
+      <Link href="/dashboard">Go To The Dashboard</Link>
+    </div>
+  );
 };
 
 export default Home;
